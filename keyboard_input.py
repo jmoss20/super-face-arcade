393fd0e8bcc7ef8b3022f53d23f42a3f4e3d834c
Sam Pochyly <sampocs@Sams-MacBook-Pro-5.local>
2017-02-19 04:42:03 -0600

    key functions

--- keyboard_input.py
+++ keyboard_input.py
@@ -1,5 +1,6 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/PyUserInput")
+import time
 
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
@@ -9,5 +10,25 @@ k = PyKeyboard()
 
 x_dim, y_dim = m.screen_size()
 m.click(x_dim/2, y_dim/2, 1)
-k.type_string('Hello, World!')
-k.press_key('H')
+
+def accelerate():
+	k.press_key('option')
+	time.sleep(0.5)
+	k.release_key('option')
+
+def turnRight():
+	k.press_key('J')
+	time.sleep(0.1)
+	k.release_key('J')
+
+def turnLeft():
+	k.press_key('G')
+	time.sleep(0.1)
+	k.release_key('G')
+
+def powerUp():
+	k.press_key('command')
+	time.sleep(0.5)
+	k.release_key('command')
+
+powerUp()
